811643450b7e4dafca8fb1b03959cd4a98d0d77a
Kate Kiatsiri <keidsiri@gmail.com>
2022-02-11 11:43:47 -0800

    Add BeerControl

---
+++ src/components/BeerControl.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import NewBeerForm from './NewBeerForm';
+import BeerList from './BeerList';
+import BeerDetail from './BeerDetail';
+
+class BeerControl extends React.Component {
+  
+}
