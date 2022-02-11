aaffb5b43ea7c5f3aa6516f73524fbf4a19228e8
Kate Kiatsiri <keidsiri@gmail.com>
2022-02-11 10:44:30 -0800

    Add Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(){
+  return (
+    <React.Fragment>
+      <h1 classname="header"> Beer's Selections</h1>
+      <br/>
+    </React.Fragment>
+  );
+
+}
+
+export default Header;
